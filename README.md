d27aba7e9e75d351751563297a01d366d9d0a80d
Itgotworse26 <alvin.tran1997@gmail.com>
2022-01-12 11:16:42 -0800

    Update readme and index.html.

--- README.md
+++ README.md
@@ -7,7 +7,6 @@ Roza has a partially completed dashboard that she needs to finish. She has a com
 ## Link
 In order to access the site, please use this [URL](https://itgotworse26.github.io/Improbable_Beef/).
 
-
 ## Commentary
 The most difficulty I had was adjusting the image on the jumbotron without using a CSS file. For Roza's project, I decided to mix things up and not use a CSS file. This meant writing syntax for the jumbotron inside the index.html file. This meant having to figure out the different syntax to adjust the side of the image that would be used for the jumbotron. In the end, I used this line of code to create the jumbotron background:
 
@@ -19,3 +18,37 @@ The most difficulty I had was adjusting the image on the jumbotron without using
 ```
 
 It allowed me to keep the image centered and fill the jumbotron. 
+
+Unfortunately, the decision to not use a CSS file meant that I had no ability to customize the font of the jumbotron. Thankfully, the image I selected still allowed for black font.
+
+Also, I was able to add more information about the project as a few sentences on the page. I experimented a little with the column number before settling on 12 so that the index.html syntax looks like this:
+
+```
+  <div class="container-fluid">
+      <div class="row">
+          <div class="col-md-12">
+              <p>
+                  Our navels, or belly buttons, plays host to multitudes of bacteria, many which are still undocumented to this day <br><br>
+                  The purpose of this project is to determine the types and populations of bacteria is living in our sample population's belly buttons. One day, we hope to find the silver bacteria that can synthesize the taste of beef.<br><br>
+                  What micro-organisms are in your belly button? As for our sample, come take a peek.
+              </p>
+          </div>
+      </div>
+  </div>
+```
+
+I was also able to add a navigation bar to allow the user to toggle the different charts with this function:
+
+```
+function changeChartVisibility(option) {
+  var element = document.getElementById(option);
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  }
+  else {
+    element.style.display = "none";
+  }
+}
+```
+
+With the use of the getElementById method, I can toggle whether the chart is displayed on the page or not. 
--- static/js/charts.js
+++ static/js/charts.js
@@ -53,36 +53,36 @@ function buildMetadata(sample) {
   });
 }
 
-// 1. Create the buildCharts function.
+// 1.1. Create the buildCharts function.
 function buildCharts(sample) {
   
-  // 2. Use d3.json to load and retrieve the samples.json file 
+  // 1.2. Use d3.json to load and retrieve the samples.json file 
   d3.json("static/data/samples.json").then((data) => {
 
     // BAR CHART
 
-    // 3. Create a variable that holds the samples array. 
+    // 1.3. Create a variable that holds the samples array. 
     var sampleArray = data.samples;
 
-    // 4. Create a variable that filters the samples for the object with the desired sample number.
+    // 1.4. Create a variable that filters the samples for the object with the desired sample number.
     var filteredSamples = sampleArray.filter(s => s.id === sample);
 
-    //  5. Create a variable that holds the first sample in the array.
+    //  1.5. Create a variable that holds the first sample in the array.
     var firstSample = filteredSamples[0];
 
-    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    // 1.6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otu_ids = firstSample.otu_ids;
     var otu_labels = firstSample.otu_labels;
     var sample_values = firstSample.sample_values;
 
-    // 7. Create the yticks for the bar chart.
+    // 1.7. Create the yticks for the bar chart.
     // Hint: Get the the top 10 otu_ids and map them in descending order  
     //  so the otu_ids with the most bacteria are last. 
     var yticks = otu_ids.slice(0, 10).reverse().map(num => "OTU " + num);
     var xticks = sample_values.slice(0, 10).reverse();
     var labels = otu_labels.slice(0, 10).reverse();
 
-    // 8. Create the trace for the bar chart. 
+    // 1.8. Create the trace for the bar chart. 
     var barData = [{
       type: 'bar',
       x: xticks,
@@ -91,18 +91,18 @@ function buildCharts(sample) {
       orientation: 'h'
     }];
 
-    // 9. Create the layout for the bar chart. 
+    // 1.9. Create the layout for the bar chart. 
     var barLayout = {
      title: 'Top 10 Bacteria Cultures Found'
     };
 
-    // 10. Use Plotly to plot the data with the layout. 
+    // 1.10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
 
 
     // BUBBLE CHART
 
-    // 1. Create the trace for the bubble chart.
+    // 2.1. Create the trace for the bubble chart.
     var bubbleData = [{
       x: otu_ids,
       y: sample_values,
@@ -114,7 +114,7 @@ function buildCharts(sample) {
       }
     }];
 
-    // 2. Create the layout for the bubble chart.
+    // 2.2. Create the layout for the bubble chart.
     var bubbleLayout = {
       title: 'Bacteria Cultures Per Sample',
       xaxis: {
@@ -123,25 +123,25 @@ function buildCharts(sample) {
       hovermode: 'closest'
     };
 
-    // 3. Use Plotly to plot the data with the layout.
+    // 2.3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
 
 
     // GAUGE CHART
 
-    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    // 3.1. Create a variable that filters the metadata array for the object with the desired sample number.
     var metadataArray = data.metadata
     var filteredMetadata = metadataArray.filter(m => m.id.toString() === sample);
 
-    // 2. Create a variable that holds the first sample in the metadata array.
+    // 3.2. Create a variable that holds the first sample in the metadata array.
     var firstMetadata = filteredMetadata[0];
     console.log(sampleArray);
     console.log(metadataArray);
 
-    // 3. Create a variable that holds the washing frequency.
+    // 3.3. Create a variable that holds the washing frequency.
     var wfreq = firstMetadata.wfreq;
 
-    // 4. Create the trace for the gauge chart.
+    // 3.4. Create the trace for the gauge chart.
     var gaugeData = [{
       type: 'indicator',
       mode: 'gauge+number',
@@ -162,18 +162,20 @@ function buildCharts(sample) {
       }
     }];
     
-    // 5. Create the layout for the gauge chart.
+    // 3.5. Create the layout for the gauge chart.
     var gaugeLayout = { 
       width: 450,
       height: 450,
       margin: { t: 0, b: 0 }
     };
 
-    // 6. Use Plotly to plot the gauge data and layout.
+    // 3.6. Use Plotly to plot the gauge data and layout.
     Plotly.newPlot("gauge", gaugeData, gaugeLayout);
   });
 }
 
+
+// 4.1. Change the chart visibility according to the navigation bar.
 function changeChartVisibility(option) {
   var element = document.getElementById(option);
   if (element.style.display === "none") {
